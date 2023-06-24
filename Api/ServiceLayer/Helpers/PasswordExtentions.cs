d4980e992c44e7bc2089d780e265b8a792f81d87
saidsn <saidsn@code.edu.az>
2023-06-24 19:03:51 +0400

    Feature: LoginDto and RegisterDto Validator

---
+++ Api/ServiceLayer/Helpers/PasswordExtentions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ServiceLayer.Helpers
+{
+    public static class PasswordExtentions
+    {
+        public static bool HasValidPassword(string pw)
+        {
+            var lowercase = new Regex("[a-z]+");
+            var uppercase = new Regex("[A-Z]+");
+            var digit = new Regex("(\\d)+");
+            var symbol = new Regex("(\\W)+");
+
+            return (lowercase.IsMatch(pw) && uppercase.IsMatch(pw) && digit.IsMatch(pw) && symbol.IsMatch(pw));
+        }
+    }
+}
--- Api/ServiceLayer/Validations/Account/RegisterDtoValidator.cs
+++ Api/ServiceLayer/Validations/Account/RegisterDtoValidator.cs
@@ -4,12 +4,15 @@ using ServiceLayer.Helpers;
 
 namespace ServiceLayer.Validations.Account
 {
-    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    public class RegisterDtoValidator : AbstractValidator<RegisterDto>
     {
-        public LoginDtoValidator()
+        public RegisterDtoValidator()
         {
+            RuleFor(u => u.FullName).NotNull().NotEmpty().MaximumLength(50);
+            RuleFor(u => u.Username).NotNull().NotEmpty().MaximumLength(50);
             RuleFor(u => u.Email).NotNull().NotEmpty().MaximumLength(50).EmailAddress();
             RuleFor(u => u.Password).NotNull().NotEmpty().Length(6, 12).Must(u => PasswordExtentions.HasValidPassword(u));
+            RuleFor(u => u.ConfirmPassword).Equal(u => u.Password);
         }
     }
 }
